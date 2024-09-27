e00d2a8ffe2eefaaa366bf02c076e6b3a9398159
arachid1 <alilerachidi@gmail.com>
2024-09-27 02:22:40 -0400

    pushing ingest of form values as parameters

--- birdshot_modeller.py
+++ birdshot_modeller.py
@@ -1,5 +1,5 @@
 from openmsimodel.interactive.gemd_modeller import GEMDModeller
-from gemd import MaterialTemplate, ProcessTemplate, MeasurementTemplate, ParameterTemplate, ConditionTemplate, MaterialRun, MaterialSpec, RealBounds, CategoricalBounds
+from gemd import MaterialTemplate, ProcessTemplate, MeasurementTemplate, ParameterTemplate, ConditionTemplate, MaterialRun, MaterialSpec, RealBounds, CategoricalBounds, Parameter, NominalReal
 from openmsimodel.structures.materials_sequence import MaterialsSequence
 from openmsimodel.science_kit.science_kit import ScienceKit
 from openmsimodel.science_kit.science_kit import ScienceKit
@@ -58,8 +58,6 @@ def find_folder_by_path(root_folder_id, folder_path):
 
 def ni_model(file_name, file_path, component):
 
-    print(file_name)
-    print(file_path)
     file_id = component['file_id_regex_pattern']
     match = re.search(file_id[0], file_path)
 
@@ -75,17 +73,15 @@ def ni_model(file_name, file_path, component):
             ni_data = form['data']
 
     print(ni_data)
+    print(ni_data['Analysis Parameters'])
 
     ni_folder_id = find_folder_by_path('66310ca21b56c53abc7e0f9f', ni_data['targetPath'])
 
-    print('folder_id', ni_folder_id)
     dms_ni_folder_items = client.get(f"/item", parameters={
         'folderId': ni_folder_id,
     })
-    print(dms_ni_folder_items)
 
     area_cag_files = [item['name'] for item in dms_ni_folder_items if item['name'].endswith('_area.cag')]
-    print(area_cag_files)
 
     science_kit = ScienceKit()
 
@@ -129,62 +125,170 @@ def ni_model(file_name, file_path, component):
     melting_sequence.link_within()
 
     #####
-    nanoindentation_sample_ingredient = Ingredient("Sample Attached with Crystal Bond")
+    def make_constant_sr_nanoindentation_experiment(name, strain_rate, ingredient_name):
+        nanoindentation_sample_ingredient = Ingredient(ingredient_name)
 
-    nanoindentation_process = Process(
-        "Nanoindentation", 
-        template=ProcessTemplate(
-            "Nanoindentation Process", 
-            parameters=[
-                ParameterTemplate(
-                    name="Indentation Depth", 
-                    bounds=RealBounds(0, 10000, "nm")  # Depth range from 0 to 10,000 nm
-                ),
-                ParameterTemplate(
-                    name="Applied Force", 
-                    bounds=RealBounds(0, 0.5, "micronewton")  # Force applied in micronewtons
-                )
-            ]
+        tip_area_coefficient_template = ParameterTemplate(
+            name="Tip Area Function Coefficient", 
+            bounds=RealBounds(0, 100, "")  
         )
-    )
+        strain_rate_template = ParameterTemplate(
+            name="Strain Rate", 
+            bounds=RealBounds(0, 100, "mm/s")
+        )
 
-    indented_sample_material = Material("Indented Sample", template=sample_material_template)
+        actuator_calibration_coefficient_template = ParameterTemplate(
+            name="Actuator Calibration Coefficient", 
+            bounds=RealBounds(0, 100, "")
+        )
 
-    depth_measurement = Measurement(
-        name="Indentation Depth Measurement", 
-        template=MeasurementTemplate(
-            name="Depth", 
-            parameters=ParameterTemplate(
+        actuator_column_mass_template = ParameterTemplate(
+            name="Actuator Column Mass", 
+            bounds=RealBounds(0, 5, "g")
+        )
+
+        actuator_spring_stiffness_template = ParameterTemplate(
+            name="Actuator Spring Stiffness", 
+            bounds=RealBounds(0, 1000, "N/m")
+        )
+
+        frame_stiffness_template = ParameterTemplate(
+            name="Frame Stiffness", 
+            bounds=RealBounds(0, 5000000, "N/m")
+        )
+
+        piezo_load_cell_stiffness_template = ParameterTemplate(
+            name="Piezo Load Cell Stiffness", 
+            bounds=RealBounds(0, 500000, "N/m")
+        )
+
+        piezo_calibration_coefficient_template = ParameterTemplate(
+            name="Piezo Calibration Coefficient", 
+            bounds=RealBounds(0, 100, "")
+        )
+
+        piezo_load_cell_damping_coefficient_template = ParameterTemplate(
+            name="Piezo Load Cell Damping Coefficient", 
+            bounds=RealBounds(0, 100, "")
+        )
+
+        piezo_load_cell_effective_mass_template = ParameterTemplate(
+            name="Piezo Load Cell Effective Mass", 
+            bounds=RealBounds(0, 5, "g")
+        )
+
+
+        nanoindentation_process = Process(
+            f"{name} 5-Indent Experiment", 
+            template=ProcessTemplate(
+                "Nanoindentation Process", 
+                parameters=[
+                    strain_rate_template,
+                    actuator_calibration_coefficient_template,
+                    actuator_column_mass_template,
+                    actuator_spring_stiffness_template,
+                    frame_stiffness_template,
+                    piezo_load_cell_stiffness_template,
+                    piezo_calibration_coefficient_template,
+                    piezo_load_cell_damping_coefficient_template,
+                    piezo_load_cell_effective_mass_template,
+                    tip_area_coefficient_template
+                ]
+            )
+        )
+
+        nanoindentation_process.update_parameters((Parameter('Strain Rate', value=NominalReal(strain_rate, 'mm/s'))) , which='both')
+        for parameter_name in ni_data['Analysis Parameters'].keys():
+            unit = 'dimensionless'
+
+            if 'Coefficient' in parameter_name:
+                if parameter_name == 'Tip Area Function Coefficients':
+                    print(ni_data['Analysis Parameters'][parameter_name])
+                    for coefficient_name in ni_data['Analysis Parameters'][parameter_name],keys():
+                        percentage = ni_data['Analysis Parameters'][parameter_name][coefficient_name]
+                        nanoindentation_process.update_parameters((Parameter(parameter_name, NominalReal(percentage, ''), template=tip_area_coefficient_template)) , which='both')
+                elif parameter_name == 'Actuator Calibration Coefficient':
+                    template = actuator_calibration_coefficient_template
+                elif parameter_name == 'Piezo Calibration Coefficient':
+                    template = piezo_calibration_coefficient_template
+                elif parameter_name == 'Piezo Load Cell Damping Coefficient':
+                    template = piezo_load_cell_damping_coefficient_template
+
+            elif 'Stiffness' in parameter_name:
+                unit = 'N/m'
+                if parameter_name == 'Actuator Spring Stiffness':
+                    template = actuator_spring_stiffness_template
+                elif parameter_name == 'Frame Stiffness':
+                    template = frame_stiffness_template
+                elif parameter_name == 'Pieze Load Cell Stiffness':
+                    template = piezo_load_cell_stiffness_template
+            elif 'Mass' in parameter_name:
+                unit = 'g'
+                if parameter_name == 'Actuator Column Mass':
+                    template = actuator_column_mass_template
+                elif parameter_name == 'Piezo Load Cell Effective Mass':
+                    template = piezo_load_cell_effective_mass_template
+            param = Parameter(parameter_name, value=NominalReal(ni_data['Analysis Parameters'][parameter_name], unit), template=template)
+            # value = NominalReal(ni_data['Analysis Parameters'][parameter_name], unit)
+            print("here")
+            print(parameter_name)
+            print(ni_data['Analysis Parameters'][parameter_name])
+            print(type(ni_data['Analysis Parameters'][parameter_name]))
+            print(unit)
+            nanoindentation_process.update_parameters((param) , which='both')
+
+        indented_sample_material = Material(f"{name} Indented Sample", template=sample_material_template)
+
+        depth_measurement = Measurement(
+            name=f"{name} Displacement Measurement", 
+            template=MeasurementTemplate(
                 name="Depth", 
-                bounds=RealBounds(0, 10000, "nm")
+                parameters=ParameterTemplate(
+                    name="Depth", 
+                    bounds=RealBounds(0, 10000, "nm")
+                )
             )
         )
-    )
 
-    force_measurement = Measurement(
-        name="Applied Force Measurement", 
-        template=MeasurementTemplate(
-            name="Force", 
-            parameters=ParameterTemplate(
+        force_measurement = Measurement(
+            name=f"{name}  Load Measurement", 
+            template=MeasurementTemplate(
                 name="Force", 
-                bounds=RealBounds(0, 500, "micronewton")
+                parameters=ParameterTemplate(
+                    name="Force", 
+                    bounds=RealBounds(0, 500, "micronewton")
+                )
             )
         )
-    )
 
-    # Define the nanoindentation sequence, linking it to the sample and adding measurements
-    nanoindentation_sequence = MaterialsSequence(
-        name="Nanoindentation Experiment on Bonded Sample",
-        science_kit=science_kit,  # Reference to the overall experimental kit you're using
-        ingredients=[nanoindentation_sample_ingredient],
-        material=indented_sample_material,  # The material being tested (attached sample)
-        process=nanoindentation_process,  # The nanoindentation process
-        measurements=[depth_measurement, force_measurement]  # Measure depth and force during the process
-    )
+        # Define the nanoindentation sequence, linking it to the sample and adding measurements
+        nanoindentation_sequence = MaterialsSequence(
+            name=f"{name} Nanoindentation Experiment on Bonded Sample",
+            science_kit=science_kit,  # Reference to the overall experimental kit you're using
+            ingredients=[nanoindentation_sample_ingredient],
+            material=indented_sample_material,  # The material being tested (attached sample)
+            process=nanoindentation_process,  # The nanoindentation process
+            measurements=[depth_measurement, force_measurement]  # Measure depth and force during the process
+        )
 
-    nanoindentation_sequence.link_within()
+        nanoindentation_sequence.link_within()
 
-    nanoindentation_sequence.link_prior(melting_sequence, ingredient_name_to_link="Sample Attached with Crystal Bond")
+        return nanoindentation_sequence
+
+    name = '100'
+    ingredient_name = f"{name} SR Attached Sample"
+    nanoindentation_calibration_sequence = make_constant_sr_nanoindentation_experiment(f'{name} SR Calibration NI', strain_rate=100, ingredient_name=ingredient_name)
+    nanoindentation_calibration_sequence.link_prior(melting_sequence, ingredient_name_to_link=ingredient_name)
+    name = '2'
+    ingredient_name = f"{name} SR Attached Sample"
+    nanoindentation_1_sequence = make_constant_sr_nanoindentation_experiment(f'{name} SR NI', strain_rate=2, ingredient_name=ingredient_name)
+    nanoindentation_1_sequence.link_prior(nanoindentation_calibration_sequence, ingredient_name_to_link=ingredient_name)
+    name = '1'
+    ingredient_name = f"{name} SR Attached Sample"
+    nanoindentation_2_sequence = make_constant_sr_nanoindentation_experiment(f'{name} SR NI', strain_rate=1, ingredient_name=ingredient_name)
+    nanoindentation_2_sequence.link_prior(nanoindentation_1_sequence, ingredient_name_to_link=ingredient_name)
+
+
 
     return science_kit.assets()
 
